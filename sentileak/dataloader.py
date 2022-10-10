8d63645161a33b7cd51d9ea54153b7c8fe2f7d2f
FernanOrtega <f.ortega.gallego@gmail.com>
2022-10-10 14:04:55 +0200

    Added new feature to allow users to use custom diccionaries.

--- sentileak/dataloader.py
+++ sentileak/dataloader.py
@@ -5,29 +5,31 @@ from typing import Dict, List
 BASE_URL = f"{os.path.dirname(os.path.realpath(__file__))}/data"
 
 
-def load_dict(language: str, file_name: str) -> Dict:
+def load_dict(language: str, file_name: str, base_url: str = BASE_URL) -> Dict:
     """
     Method to load dictionaries from a file
     :param language: language of the dictionary to load
     :param file_name: name of the file to load
+    :param base_url: url base
     :return: a dictionary with the file content
     """
 
-    with open(os.path.join(BASE_URL, language, file_name)) as csvfile:
+    with open(os.path.join(base_url, language, file_name)) as csvfile:
         csv_reader = csv.reader(csvfile)
         result = {row[0]: float(row[1]) for row in csv_reader}
 
     return result
 
 
-def load_string_file(language: str, file_name: str) -> List[str]:
+def load_string_file(language: str, file_name: str, base_url: str = BASE_URL) -> List[str]:
     """
     Method to load a list of string that represents the lines of a file
     :param language: language of the file to load
     :param file_name: name of the file to load
     :return: a list with the lines of the file
+    :param base_url: url base
     """
-    with open(os.path.join(BASE_URL, language, file_name)) as str_file:
+    with open(os.path.join(base_url, language, file_name)) as str_file:
         result = [line.replace("\n", "") for line in str_file]
 
     return result
